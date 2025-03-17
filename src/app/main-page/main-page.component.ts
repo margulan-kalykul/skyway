6fddb2f7357677f0749280acfbce9d379b775a9c
margulan-kalykul <m_kalykul@kbtu.kz>
2025-03-17 11:35:20 +0500

    Fixed som main page alignments

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,26 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TourScheduleComponent } from '../tour-schedule/tour-schedule.component';
 import { AllToursComponent } from '../all-tours/all-tours.component';
 import { Router, RouterLink } from '@angular/router';
 import { DeviceService } from '../services/device.service';
 import { DeviceInfo } from '../models/device-info';
+import { TopOfPageComponent } from "../top-of-page/top-of-page.component";
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [TourScheduleComponent, AllToursComponent, RouterLink],
+  imports: [TourScheduleComponent, AllToursComponent, TopOfPageComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
   deviceInfo: DeviceInfo;
 
-  constructor(private router: Router, deviceService: DeviceService) {
+  constructor(deviceService: DeviceService) {
     this.deviceInfo = deviceService.getClientInfo();
     console.log('Client Info:', this.deviceInfo);
   }
 
-  sign_in(): void {
-    this.router.navigate(['/sign-in']);
+  ngOnInit(): void {
+    let topOfPage = document.getElementById("top-of-page");
+    console.log(topOfPage);
+    if (topOfPage != null) {
+      topOfPage.style.width = `${this.deviceInfo.viewScreen.width}px`;
+      // topOfPage.style.height = `${this.deviceInfo.viewScreen.height}px`;
+      console.log(`${topOfPage.style.width} ${topOfPage.style.height} something`);
+    }
   }
 }
