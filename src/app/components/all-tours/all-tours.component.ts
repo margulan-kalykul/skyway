e3cc32a121f050c42b82ef8eee8c7cb1d58e6c24
margulan-kalykul <m_kalykul@kbtu.kz>
2025-03-26 12:40:59 +0500

    Finised multi tour cards

--- src/app/components/all-tours/all-tours.component.ts
+++ src/app/components/all-tours/all-tours.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { TourCardComponent } from '../tour-card/tour-card.component';
 import { ToursService } from '../../services/tours.service';
 import { Tour, TourOld } from '../../models/interfaces';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-all-tours',
   standalone: true,
-  imports: [TourCardComponent],
+  imports: [NgFor, TourCardComponent],
   templateUrl: './all-tours.component.html',
   styleUrl: './all-tours.component.css'
 })
@@ -14,20 +15,21 @@ export class AllToursComponent implements OnInit {
   // tours: TourOld[][];
   tours: Tour[];
   hiddenTours: Tour[];
+  multiTours: Tour[];
   headers_list: string[];
+  currentPart = 0;
+  isRightArrowShown = true;
+  isLeftArrowShown = false;
 
   constructor(private toursService: ToursService) {
     this.tours = [];
     this.hiddenTours = [];
+    this.multiTours = [];
     this.headers_list = [];
   }
 
   ngOnInit(): void {
     this.getTours();
-    if (this.tours.length > 6) {
-      this.hiddenTours = this.tours.slice(6);
-      this.tours = this.tours.slice(0, 6);
-    }
     if (this.tours.length > 6) {
       this.hiddenTours = this.tours.slice(6);
       this.tours = this.tours.slice(0, 6);
@@ -40,11 +42,51 @@ export class AllToursComponent implements OnInit {
     this.hiddenTours = this.hiddenTours.slice(6);
   }
 
+  nextMultiTours(): void {
+    if (this.currentPart < this.multiTours.length-4) {
+      this.currentPart++;
+    }
+    if (this.currentPart === this.multiTours.length-4) {
+      this.isRightArrowShown = false;
+      this.isLeftArrowShown = true;
+    }
+    else {
+      this.isRightArrowShown = true;
+      this.isLeftArrowShown = false;
+    }
+  }
+
+  prevMultiTours(): void {
+    if (this.currentPart > 0) {
+      this.currentPart--;
+    }
+    if (this.currentPart === 0) {
+      this.isLeftArrowShown = false;
+      this.isRightArrowShown = true;
+    }
+    else { 
+      this.isLeftArrowShown = true;
+      this.isRightArrowShown = false;
+    }
+  }
+
   getTours(): void {
     // this.toursService.getToursWithHeaders().subscribe((toursWithHeaders) => {
     //   this.tours = toursWithHeaders;
     // })
     this.tours = [
+      {id: '1', description: 'Desc1', owner_id: '1', route: 'Route1', tour_categories: null, tour_events: null, tour_images: null, tour_location: null, tour_videos: null},
+      {id: '1', description: 'Desc1', owner_id: '1', route: 'Route2', tour_categories: null, tour_events: null, tour_images: null, tour_location: null, tour_videos: null},
+      {id: '1', description: 'Desc1', owner_id: '1', route: 'Route3', tour_categories: null, tour_events: null, tour_images: null, tour_location: null, tour_videos: null},
+      {id: '1', description: 'Desc1', owner_id: '1', route: 'Route4', tour_categories: null, tour_events: null, tour_images: null, tour_location: null, tour_videos: null},
+      {id: '1', description: 'Desc1', owner_id: '1', route: 'Route5', tour_categories: null, tour_events: null, tour_images: null, tour_location: null, tour_videos: null},
+      {id: '1', description: 'Desc1', owner_id: '1', route: 'Route6', tour_categories: null, tour_events: null, tour_images: null, tour_location: null, tour_videos: null},
+      {id: '1', description: 'Desc1', owner_id: '1', route: 'Route7', tour_categories: null, tour_events: null, tour_images: null, tour_location: null, tour_videos: null},
+      {id: '1', description: 'Desc1', owner_id: '1', route: 'Route8', tour_categories: null, tour_events: null, tour_images: null, tour_location: null, tour_videos: null},
+      {id: '1', description: 'Desc1', owner_id: '1', route: 'Route9', tour_categories: null, tour_events: null, tour_images: null, tour_location: null, tour_videos: null},
+      {id: '1', description: 'Desc1', owner_id: '1', route: 'Route10', tour_categories: null, tour_events: null, tour_images: null, tour_location: null, tour_videos: null},
+    ];
+    this.multiTours = [
       {id: '1', description: 'Desc1', owner_id: '1', route: 'Route1', tour_categories: null, tour_events: null, tour_images: null, tour_location: null, tour_videos: null},
       {id: '1', description: 'Desc1', owner_id: '1', route: 'Route1', tour_categories: null, tour_events: null, tour_images: null, tour_location: null, tour_videos: null},
       {id: '1', description: 'Desc1', owner_id: '1', route: 'Route1', tour_categories: null, tour_events: null, tour_images: null, tour_location: null, tour_videos: null},
