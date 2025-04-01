deb23051bf8151a6c755f1607008202653ad2afb
margulan-kalykul <m_kalykul@kbtu.kz>
2025-04-01 20:59:30 +0500

    Tour card fix ellipsis (not finished, not working yet)

--- src/app/components/tour-card/tour-card.component.ts
+++ src/app/components/tour-card/tour-card.component.ts
@@ -11,17 +11,57 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './tour-card.component.css'
 })
 export class TourCardComponent implements OnInit {
-  @Input() tour!: Tour;
+  @Input() tour: Tour = {
+    id: '',
+    description: '',
+    owner_id: '',
+    route: '',
+    tour_categories: null,
+    tour_events: null,
+    tour_images: null,
+    tour_location: null,
+    tour_videos: null
+  };
   @Input() userId!: number | null;
+  ellipsedDesc: string;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(private router: Router, private authService: AuthService) {
+    // TODO: fetch
+    this.tour.description = 'TOP natural landmarks of Almaty - Charyn Canyon, Black and Moon Canyons, Kolsay and Kaindy Lakes';
+    // Default value
+    this.ellipsedDesc = this.tour.description;
+  }
 
   ngOnInit(): void {
-    
+    this.tour.description = 'TOP natural landmarks of Almaty - Charyn Canyon, Black and Moon Canyons, Kolsay and Kaindy Lakes';
+    this.ellipsedDesc = '';
+    this.tour.description.replaceAll('\n', ' ');
+    let words = this.tour.description.split(' ');
+    let letters = 0, lines = 0, limit = 36;
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      console.log(word, letters, lines);
+      if (lines == 2) {
+        limit = 31;
+      }
+      if (lines == 3) {
+        this.ellipsedDesc += '...';
+      }
+      else {
+        this.ellipsedDesc += word + ' ';
+      }
+      if (letters + word.length >= limit) {
+        lines++;
+        letters = 0;
+      }
+      else {
+        letters += word.length;
+      }
+    }
+    console.log(this.ellipsedDesc);
   }
 
   favoritesClicked(): void {
-    console.log(this.userId);
     if (this.userId != null) {
       this.router.navigate(['/user', this.userId, 'favorites']);
     }
