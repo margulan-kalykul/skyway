6bfeeef841cfe23d5d1b0c8937ac8b144a34a3b2
margulan-kalykul <m_kalykul@kbtu.kz>
2025-04-08 23:14:47 +0500

    Purchase page layout

--- src/app/pages/purchase/purchase.component.ts
+++ src/app/pages/purchase/purchase.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-purchase',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ReactiveFormsModule, FooterComponent],
   templateUrl: './purchase.component.html',
   styleUrl: './purchase.component.css'
 })
 export class PurchaseComponent {
-
+  imageNames = {
+    clock: "assets/images/clock.svg",
+    people: "assets/images/users.svg",
+    globe: "assets/images/language.svg",
+  };
+  cardForm: FormGroup;
+  
+  constructor(private fb: FormBuilder) {
+    this.cardForm = this.fb.group({
+      
+    });
+  }
 }
