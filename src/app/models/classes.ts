c2e0157c40b098ef168a934936b9105e03cd68db
NurmagambetovBakytzhan <nurmagambetovbakytzan@gmail.com>
2025-06-03 17:52:50 +0500

    chat fixed and tour details modified

--- src/app/models/classes.ts
+++ src/app/models/classes.ts
@@ -15,10 +15,10 @@
 // }
 
 export class WebsocketMessage {
-    userId: string;
+    userId: string | null;
     message: string;
 
-    constructor(user: string, message: string) {
+    constructor(user: string | null, message: string) {
         this.userId = user;
         this.message = message;
     }
